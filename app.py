6fcf9495146bf3c59c41724a646422d258e87ff2
Vladimir S. Lavrik <vlavrik@Vladimirs-MacBook-Air.local>
2020-12-29 14:38:47 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,27 @@
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
+import dash_bootstrap_components as dbc
+
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+app.layout = html.Div([
+    html.Div(
+        className="app-header",
+        children=[
+            html.Div('Analize your car', className="app-header--title")
+        ]
+    ),
+    html.Div(
+        children=html.Div([
+            html.H5('Overview'),
+            html.Div('''
+                This is an example of a simple Dash app with
+                local, customized CSS.
+            ''')
+        ])
+    )
+], className='main')
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
